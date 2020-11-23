145f4d3e56ce12362bf024b0f626303b62462eda
zhanwei.lzw <zhanwei.lzw@alibaba-inc.com>
2020-11-23 17:47:04 +0800

    ios setting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dy/main.dart';
+import 'package:barrage_video/main.dart';
 
 void main() {}
